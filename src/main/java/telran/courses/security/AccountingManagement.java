24a5cc890e035e3d191ce26345e648e7a19d1bd0
korsa <korsa@Dellchik>
2022-04-24 19:20:03 +0300

    commits basic auth

--- src/main/java/telran/courses/security/AccountingManagement.java
+++ src/main/java/telran/courses/security/AccountingManagement.java
@@ -2,10 +2,11 @@ package telran.courses.security;
 
 import org.springframework.stereotype.Component;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class AccountingManagement {
-	private Map<String, Account> accounts = new HashMap<>();
+	private Map<String, Account> accounts = new ConcurrentHashMap<>();
 	public AccountingManagement() {
 		accounts.put("admin@tel-ran.co.il", new Account("admin@tel-ran.co.il", "$2a$10$0d.gqun7BTHuD1lNHDNWAujVXkHwcpZIXGiXb8oJvA/JbJjfKcrpm", "ADMIN"));
 		accounts.put("user@tel-ran.co.il", new Account("user@tel-ran.co.il", "$2a$10$rSdI0lSvHmwhzOxLQ1olOujYO4gIGgRhst03Si3vKxtpASI/4W3Ni", "USER"));
